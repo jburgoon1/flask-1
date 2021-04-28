7913c0724cc70de25cd524cba3727c295d44d8ee
jburgoon1 <77408299+jburgoon1@users.noreply.github.com>
2021-04-28 08:30:49 -0600

    second

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ def show_exchanger():
 
 @app.route('/convert', methods = ['GET','POST'])
 def convert_currency():
-    input_code = request.form.get('in')
-    output_code = request.form.get('out')
-    amount = request.values.get('amount', type = int)
-    converted = converter(input_code,output_code,amount)
-    symbol = CC.get_symbol(output_code)
-    if output_code not in CR.get_rates(input_code):
-        flash('no good')
-        return redirect(url_for('show_exchanger'))
+    error = None
+    if request.method == "POST":
+        try:
+            input_code = request.form.get('in')
+            output_code = request.form.get('out')
+            amount = request.values.get('amount', type = int)
+            converted = converter(input_code,output_code,amount)
+            symbol = CC.get_symbol(output_code)
+        except:
+            error = 'Not valid symbol'
+     
+            return render_template('amount.html', converted = converted, symbol = symbol, error = error)
     flash('Currency Converted')
     return render_template('amount.html', converted = converted, symbol = symbol)
